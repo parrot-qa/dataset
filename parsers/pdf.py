ca523dd653dfe4f062d39b73298e0a1d4b24e676
hw <huangwj0726@126.com>
2022-04-12 22:03:08 -0700

    add pdf parser

--- parsers/pdf.py
+++ parsers/pdf.py
@@ -1,8 +1,34 @@
+import os
+from io import StringIO 
+from pdfminer.high_level import extract_pages
+from pdfminer.layout import LTTextContainer
+from pickletools import OpcodeInfo
 
 def extract_text(path, *args, **kwargs) -> list[dict]:
     """Take a file path as input, and return a list of text with headings."""
 
-    return []
+    paragraph_text = []
+    for page_layout in extract_pages(path):
+        for element in page_layout:
+            if isinstance(element, LTTextContainer):
+                paragraph_text.append(clean_paragraph(element.get_text()))
+    title = os.path.splitext(path)[0].split("/")[-1]
+    
+    return to_json(title, paragraph_text) 
+
+
+def clean_paragraph(paragraph_text):
+    return " ".join(paragraph_text.split())
+
+
+def to_json(title, paragraph_text):
+    # paragraph_text = pdf_extractor(os.path.join(pdf_folder, pdf_name))
+    contents = [{'tag': 'plain', 'text': text} for text in paragraph_text]
+    pdf_json = {
+        "title": title,
+        "contents": contents
+    }
+    return pdf_json
 
 
 if __name__ == '__main__':
