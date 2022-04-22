1ec9ddbbcd36cc3576308ba66b3e9054f6660c08
Vinay Gupta <vinay.23.gupta@outlook.com>
2022-04-21 20:02:09 -0700

    Redesign HTML document generator to use dynamic tree

--- parsers/html.py
+++ parsers/html.py
@@ -1,9 +1,31 @@
+from dataclasses import dataclass, field
 import re
 
 from bs4 import BeautifulSoup
 
 
 VALID_TAGS = re.compile(r'^(?:h\d|p|span|ul|ol|pre|code|table)$')
+DOCUMENT_PARTITION_LENGTH = 100
+
+
+@dataclass
+class Section:
+    level: int
+    header: str
+    contents: list = field(default_factory=list)
+    children: list = field(default_factory=list)
+    length: int = None
+
+    def compute_length(self):
+        length = sum(
+            len(c['text']) for c in self.contents
+        )
+
+        for child in self.children:
+            length += child.compute_length()
+
+        self.length = length
+        return length
 
 
 def _get_root(soup):
@@ -51,54 +73,71 @@ def _flatten_table(root):
     return text
 
 
-def add_titles(content, prefix=None, max_depth=6):
-    """Convert a flat list of headers and paragraphs into titled documents."""
+def _standardize_tag(tag):
+    if tag in {'ul', 'ol'}:
+        return 'list'
+    elif tag in {'pre', 'code'}:
+        return 'code'
+    elif tag == 'table':
+        return 'table'
+    else:
+        return 'plain'
 
-    docs = []
 
-    stack = [prefix] if prefix else []
-    level = []
+def embed_hierarchy(content):
+    """Convert a flat list of headers and paragraphs into a tree of sections."""
+
+    stack = [Section(0, None)]
+
     for c in content:
-        h = re.match(rf'h([1-{max_depth}])', c[0])
-        if h:
-            h = int(h.group(1))
-            while len(level) and level[-1] >= h:
+        if header := re.match(r'h(\d)', c[0]):
+            level = int(header.group(1))
+            while len(stack) and stack[-1].level >= level:
                 stack.pop()
-                level.pop()
-            stack.append(c[1])
-            level.append(h)
+            section = Section(level, c[1])
+            stack[-1].children.append(section)
+            stack.append(section)
         else:
-            if len(stack) == 0:
-                # Don't capture text before the first heading
-                continue
-            docs.append((tuple(stack), *c))
+            if len(stack) > 1:
+                # Only capture text after the first header
+                stack[-1].contents.append({
+                    'tag': _standardize_tag(c[0]),
+                    'text': c[1]
+                })
 
-    return docs
-
-
-def group_sections(sections):
-    parts = []
-
-    last_title = None
-    for title, tag, text in sections:
-        if title != last_title:
-            parts.append({'title': ' '.join(title), 'contents': []})
-            last_title = title
-
-        if tag in {'ul', 'ol'}:
-            ptag = 'list'
-        elif tag in {'pre', 'code'}:
-            ptag = 'code'
-        elif tag == 'table':
-            ptag = 'table'
-        else:
-            ptag = 'plain'
-        parts[-1]['contents'].append({'tag': ptag, 'text': text})
-
-    return parts
+    stack[0].compute_length()
+    return stack[0]
 
 
-def extract_text(path, max_depth=2) -> list[dict]:
+def partition_docs(node, threshold=DOCUMENT_PARTITION_LENGTH, prefix=[]):
+    doc = []
+    next_prefix = prefix + [node.header] if node.header else prefix
+
+    if node.contents:
+        doc.append({'title': ': '.join(next_prefix), 'contents': node.contents})
+    if len(node.children) == 0:
+        return doc
+
+    if any(child.length < threshold for child in node.children):
+        # Children content is too short to be used individually, merge them
+        if len(doc) == 0:
+            doc.append({'title': ': '.join(next_prefix), 'contents': []})
+        for child in node.children:
+            for subdoc in partition_docs(child, prefix=next_prefix):
+                doc[0]['contents'].append({'tag': 'plain', 'text': subdoc['title']})
+                doc[0]['contents'].extend(subdoc['contents'])
+    else:
+        for child in node.children:
+            doc.extend(partition_docs(child, prefix=next_prefix))
+
+    return doc
+
+
+# TODO:
+# - Verify that extra whitespace in all documents are removed (both HTML and PDF)
+# - Test for PPTs, and books which gave errors or very short documents earlier.
+
+def extract_text(path, threshold=DOCUMENT_PARTITION_LENGTH) -> list[dict]:
     """Take a file path as input, and return a list of text with headings."""
 
     with open(path, 'r', encoding='utf-8') as fp:
@@ -119,13 +158,14 @@ def extract_text(path, max_depth=2) -> list[dict]:
         elif tag.name == 'table':
             text = _flatten_table(tag)
         else:
+            # TODO: Review this, if it creates white space?
             text = ''.join(tag.strings)
         if not text:
             continue
         results.append((tag.name, text))
 
-    sections = add_titles(results, max_depth=max_depth)
-    sections = group_sections(sections)
+    doctree = embed_hierarchy(results)
+    sections = partition_docs(doctree, threshold=threshold)
     return sections
 
 
@@ -136,8 +176,29 @@ def extract_text_basic(html):
 
 
 if __name__ == '__main__':
-    sections = extract_text('.cache/materials/DS100/1_1__The_Students_of_Data_100.html')
+    with open('playground.html', 'w') as fp:
+        fp.write("""
+            <main>
+            <p>This data should be lost.</p>
+            <h1>Project</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            <h2>Grading</h2>
+            <h3>Peer Option</h3>
+            <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+            <h3>Tutor Option</h3>
+            <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+            <h2>Submission</h2>
+            <p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            <h3>Timeline</h3>
+            <p>...tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat...</p>
+            <h3>Breakup</h3>
+            <p>15 points.</p>
+            </main>
+        """)
+    sections = extract_text('playground.html')
     assert type(sections) == list
 
     text = extract_text_basic('<p>\nHello!\n<b> How are you doing? </b>\n</p>')
-    print(text)
+    assert text == 'Hello! How are you doing?'
+
+    print('All unit tests passed.')
